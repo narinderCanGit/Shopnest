7cdfd8e17e6b323c63dc802bfa9fe73fc14e23e3
narinderCanGit <narinder111198@gmail.com>
2024-03-30 22:40:13 +0530

    Add product controller and error middleware

--- backend/middleware/asyncHandler.js
+++ backend/middleware/asyncHandler.js
@@ -1,4 +1,4 @@
-const asyncHandler = fn => (req, res, next) => {
+const asyncHandler = fn => (req, res, next) => {     // To avoid try catch block
     Promise.resolve(fn(req, res, next)).catch(next);
 }
 
