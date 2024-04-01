e178b8ec745e817ffde25304fe7e3a95858921ac
narinderCanGit <narinder111198@gmail.com>
2024-04-01 14:16:37 +0530

    Spinner and Alert component

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -2,6 +2,8 @@ import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 
 const ProductScreen = () => {
@@ -15,8 +17,8 @@ const ProductScreen = () => {
       </Link>
 
       {
-        isLoading ? (<h2>Loading...</h2>) : error ? (
-          <div>{error?.data?.message || error?.error}</div>
+        isLoading ? <Loader/> : error ? (
+          <Message variant= 'danger'>{error?.data?.message || error?.error}</Message>
         ) : (
           <Row>
             <Col md={5}>
