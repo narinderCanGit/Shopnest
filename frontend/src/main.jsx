9a677bb66abe0a671a018b1b6e080be6aa06a51c
narinderCanGit <narinder111198@gmail.com>
2024-04-06 16:49:19 +0530

    FE: Form container and Login screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,14 +13,15 @@ import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>} >
       <Route index={true} path='/' element={<HomeScreen/>} />
       <Route path='/product/:id' element={<ProductScreen/>} />  
-      <Route path='/cart' element={<CartScreen/>} />  
-
+      <Route path='/cart' element={<CartScreen/>} /> 
+      <Route path='/login' element={<LoginScreen/>} /> 
     </Route>
   )
 );
