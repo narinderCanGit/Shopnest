a7c1ba8411c3beb6d27530892233cc40bf7b2bed
narinderCanGit <narinder111198@gmail.com>
2024-04-06 14:41:24 +0530

    Auth slice and usersAPI slice

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,16 @@
+import { apiSlice } from './apiSlice';
+import { USERS_URL } from '../constants';
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({           // mutation is used since this is not a get request
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = userApiSlice;
