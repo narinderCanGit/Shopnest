3f14c0b2ea21b6c319ab81d7119dd25c8991d918
narinderCanGit <narinder111198@gmail.com>
2024-04-07 22:07:50 +0530

    Login functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,20 @@
-import { useSelector } from 'react-redux';
-import {Navbar, Nav, Container, Badge} from 'react-bootstrap';
-import {FaShoppingCart, FaUser} from 'react-icons/fa';
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png';
 
 const Header = () => {
-  const {cartItems} = useSelector((state) => state.cart);
+  const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const logoutHandler = () => {
+    console.log('logout');
+  };
+
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
             <Navbar.Brand>
@@ -16,25 +22,38 @@ const Header = () => {
               ShopNest
             </Navbar.Brand>
           </LinkContainer>
-          {/* react-router-bootstrap - to implement routes in bootstrap component (href -> to with LinkContainer) */}
-          <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
+            {/* react-router-bootstrap - to implement routes in bootstrap component (href -> to with LinkContainer) */}
             <Nav className='ms-auto'>
               <LinkContainer to='/cart'>
                 <Nav.Link>
-                  <FaShoppingCart/> Cart
-                  {
-                    cartItems.length>0 && (<Badge pill bg='success' style={{marginLeft: '5px'}}>
-                      {
-                        cartItems.reduce((acc, curr) => acc + curr.qty, 0 )
-                      }
-                    </Badge>)
-                  }
+                  <FaShoppingCart /> Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/login'>
-                <Nav.Link><FaUser/> Sign In </Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <FaUser /> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -42,5 +61,4 @@ const Header = () => {
     </header>
   );
 };
-
 export default Header;
