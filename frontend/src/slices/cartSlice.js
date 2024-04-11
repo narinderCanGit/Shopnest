ffc42fc2271cc7312599bcb7b4a891542beddc2d
narinderCanGit <narinder111198@gmail.com>
2024-04-11 16:21:23 +0530

    FE: Shipping Screen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { updateCart } from '../utils/cartUtils';
 
 const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) 
-  : {cartItems: []};
+  : {cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal'};
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -24,9 +24,13 @@ const cartSlice = createSlice({
       state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
       return updateCart(state);
     },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      return updateCart(state);
+    },
   },
 }); 
 
-export const {addToCart, removeFromCart} = cartSlice.actions;
+export const {addToCart, removeFromCart, saveShippingAddress} = cartSlice.actions;
 
 export default cartSlice.reducer;
